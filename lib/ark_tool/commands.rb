08cd1b48cd3fdcc9a5ca4cf4ff74817900a9224b
Donovan Young <dyoung522@gmail.com>
2017-05-05 15:27:31 -0400

    added ModInstaller and Configs

--- lib/ark_tool/commands.rb
+++ lib/ark_tool/commands.rb
@@ -3,14 +3,27 @@ require "thor"
 
 module ArkTools
   class Commands < Thor
+    desc "ActiveMods FILE", "Creates an ActiveMods= line suitable for GameUserSettings.ini"
+    def activemods(file)
+      puts "ActiveMods=#{ArkModList.new(file).csv}"
+    end
+
+    desc "Configs FILE", "Generates both ActiveMods and ModInstaller"
+    def configs(file)
+      invoke :activemods, file
+      puts
+      invoke :modinstaller, file
+    end
+
     desc "ListMods FILE", "Displays a list of active mods from the provided input file"
     def listmods(file)
       ArkModList.new(file).each { |mod| printf "%-25s %s\n", mod.description, mod.id }
     end
 
-    desc "ActiveMods FILE", "Creates an ActiveMods= line suitable for GameUserSettings.ini"
-    def activemods(file)
-      puts "ActiveMods=#{ArkModList.new(file).csv}"
+    desc "ModInstaller FILE", "Creates a [ModInstaller] block suitable for Game.ini"
+    def modinstaller(file)
+      puts "[ModInstaller]"
+      ArkModList.new(file).each { |mod| printf "ModIds=%s ; %s\n", mod.id, mod.description }
     end
   end # class Config
 end # module ArkTools
