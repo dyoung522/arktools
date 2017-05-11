9f67d700d23737ad670b199b1689bc69056c03b0
Donovan Young <dyoung522@gmail.com>
2017-05-11 18:18:34 -0400

    refactor mod specs

--- spec/ark_tool/mods_spec.rb
+++ spec/ark_tool/mods_spec.rb
@@ -18,24 +18,30 @@ module ArkTools
     end
 
     describe ArkModList do
-      let (:modlist) { ArkModList.new({ input: "spec/modlist_test" }) }
+      let (:modlist) { ArkModList.new({ input: "testfile" }) }
+      let (:output) { StringIO.new("Foo\n#11111 comment 1\n22222 test 1\n;55555 comment 2\n44444 test 2\n33333 test 3\n") }
+
+      before(:each) do
+        allow(File).to receive(:open).with("testfile", "r").and_yield(output)
+      end
 
       it "instantiates a new class" do
         expect(modlist).to be_an_instance_of ArkModList
       end
 
       it "ignores invalid/commented lines" do
-        expect(modlist.mods.length).to eq(2)
+        expect(modlist.mods.length).to eq(3)
       end
 
       it "maintains modlist order" do
-        expect(modlist.mods[0].id).to eq("12345")
-        expect(modlist.mods[1].id).to eq("56789")
+        expect(modlist.mods[0].id).to eq("22222")
+        expect(modlist.mods[1].id).to eq("44444")
+        expect(modlist.mods[2].id).to eq("33333")
       end
 
       it "responds to #csv" do
         expect(modlist).to respond_to(:csv)
-        expect(modlist.csv).to eq("12345,56789")
+        expect(modlist.csv).to eq("22222,44444,33333")
       end
       end
   end
